2de96085644eb378c8683f8085b3c46bc953e11b
Steve Foster <sfoster2008@cox.net>
2018-04-19 06:55:17 -0700

    update files with content

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Salacious Crumb',
+		photo:'http://www.orlandosentinel.com/entertainment/star-wars/os-greatest-star-wars-characters-pictures-photogallery.html,
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Sebulba',
+		photo: 'http://www.orlandosentinel.com/entertainment/star-wars/os-greatest-star-wars-characters-pictures-photogallery.html',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Bossk',
+		photo: 'http://www.orlandosentinel.com/entertainment/star-wars/os-greatest-star-wars-characters-pictures-photogallery.html',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Watto',
+		photo: 'http://www.orlandosentinel.com/entertainment/star-wars/os-greatest-star-wars-characters-pictures-photogallery.html',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Bib Fortuna',
+		photo: 'http://www.orlandosentinel.com/entertainment/star-wars/os-greatest-star-wars-characters-pictures-photogallery.html',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Wickett',
+		photo: 'http://www.orlandosentinel.com/entertainment/star-wars/os-greatest-star-wars-characters-pictures-photogallery.html',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Admiral Akbar',
+		photo: 'http://www.orlandosentinel.com/entertainment/star-wars/os-greatest-star-wars-characters-pictures-photogallery.html',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'General Grievous',
+		photo: 'http://www.orlandosentinel.com/entertainment/star-wars/os-greatest-star-wars-characters-pictures-photogallery.html',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Lando Calrissian',
+		photo: 'http://www.orlandosentinel.com/entertainment/star-wars/os-greatest-star-wars-characters-pictures-photogallery.html',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Darth Maul',
+		photo: 'http://www.orlandosentinel.com/entertainment/star-wars/os-greatest-star-wars-characters-pictures-photogallery.html',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
